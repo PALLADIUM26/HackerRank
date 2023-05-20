c594cbf5f0d54a76475b7d1066b8f5e42fe7178a
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-05-20 10:28:28 +0530

    Create JavaIntToString.java

---
+++ Java/JavaIntToString.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaIntToString {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        String s="";
+        int flag=0;
+        if (n<=100 && n>=-100){
+            String s1=String.valueOf(n);
+            flag=1;
+            }
+        if (flag==1)
+            System.out.println("Good job");
+        else
+            System.out.println("Wrong answer");
+    }
+}
