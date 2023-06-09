cb86ce7923cde159e4887abca687d37d82c8cdd7
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-10 00:18:10 +0530

    Update JavaStringReverse.java

--- Java/JavaStringReverse.java
+++ Java/JavaStringReverse.java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.util.*;
 
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String rev="";
+        for (int i=A.length()-1;i>-1;i--){
+            rev+=A.charAt(i);
+        }
+        // System.out.println(rev);
+        if(A.equals(rev))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
