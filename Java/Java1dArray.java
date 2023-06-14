4cfa37d63f8b7d164f4a99295c673795658d6bff
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-15 01:01:22 +0530

    Update Java1dArray.java

--- Java/Java1dArray.java
+++ Java/Java1dArray.java
@@ -1,1 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+
+        // List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        //     .map(Integer::parseInt)
+        //     .collect(toList());
+
+        // bufferedReader.close();
+    }
+}
 
