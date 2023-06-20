20e3ac697ffbc86253c9fe7feb3d53ba2e3390fd
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-21 01:27:31 +0530

    Update PointersInC.c

--- C/PointersInC.c
+++ C/PointersInC.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int flag = 0;
+    int c = *a;
+    if(*a>*b)
+        flag = 1;
+        
+    *a = *a + *b;
+    
+    if(flag==1)
+        *b = c - *b;
+    else
+        *b = *b - c;
+}
+
+int main() {
+    int a, b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    update(&a,&b);
+    printf("%d\n",a);
+    printf("%d\n",b);
+}
 
