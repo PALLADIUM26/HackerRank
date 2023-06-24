02ec4e5a39d32fb1d3fe0c043b0c779d9bed8f43
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-25 01:36:00 +0530

    Update JavaStaticInitializerBlock.java

--- Java/JavaStaticInitializerBlock.java
+++ Java/JavaStaticInitializerBlock.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        // int l,b;
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+        // boolean flag;
+
+        if(l<1 || b<1){
+        //  flag=false;
+         System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        else{
+        //   flag=true;
+           System.out.println(l*b); 
+        }
+    }
+}
 
