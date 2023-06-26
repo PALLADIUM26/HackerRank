c0c47b1d3ddfad1b9abcbe23a627e28570793c8a
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-27 02:20:52 +0530

    Update JavaLoopsII.java

--- Java/JavaLoopsII.java
+++ Java/JavaLoopsII.java
@@ -1,1 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int arr[][]=new int[t][3];
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            arr[i][0]=a;
+            arr[i][1]=b;
+            arr[i][2]=n;
+        }
+        int term=0;
+        for (int i=0;i<t;i++){
+            for (int j=1;j<=arr[i][2];j++){
+                term=arr[i][0];
+                for (int k=1;k<=(int)Math.pow(2,j-1);k*=2){
+                    term+=arr[i][1]*k;
+                }
+                System.out.print(term+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
 
