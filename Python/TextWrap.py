b9ab6969a02b48eaac1667c6ef2978f96d8afb2d
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-29 03:21:30 +0530

    Update TextWrap.py

--- Python/TextWrap.py
+++ Python/TextWrap.py
@@ -1,1 +1,13 @@
 
+
+
+def wrap(string, max_width):
+    s=''
+    c=0
+    while c<len(string) :
+        # print(string[c:c+max_width])
+        # l.append(string[c:c+max_width])
+        s=s+'\n'+string[c:c+max_width]
+        c+=max_width
+    return s[1:]
+
