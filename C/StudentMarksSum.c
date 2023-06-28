8e32b75790e1d73cd41d2cd25d1feefc7002a77d
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-28 06:06:31 +0530

    Update StudentMarksSum.c

--- C/StudentMarksSum.c
+++ C/StudentMarksSum.c
@@ -1,1 +1,22 @@
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int i;
+  int sumB = 0;
+  int sumG = 0;
+  for(i=0; i<number_of_students; i++){
+      if(i%2 == 0){
+        sumB += marks[i];
+      }
+      else {
+        sumG += marks[i];
+      }
+  }
+  if(gender == 'b')
+    return sumB;
+  else
+    return sumG;
+}
+
 
