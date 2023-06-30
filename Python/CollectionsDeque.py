b160ddaf1da7bc36c2fce3cd4b7ef9fd3867a201
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-07-01 00:28:02 +0530

    Update CollectionsDeque.py

--- Python/CollectionsDeque.py
+++ Python/CollectionsDeque.py
@@ -1,1 +1,24 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+l=[]
+l2=[]
+n=int(input())
+for i in range(n):
+    exp=input()
+    if len(exp.split())==2:
+        l.append([exp.split()[0],int(exp.split()[1])])
+    else:
+        l.append(exp)
+    
+for i in l:
+    if 'append' in i:
+        l2.append(i[1])
+    elif 'appendleft' in i:
+        l2.insert(0,i[1])
+    elif 'popleft' in i:
+        l2.pop(0)
+    elif 'pop' in i:
+        l2.pop()
+
+for i in l2:
+    print(i,end=' ')
 
