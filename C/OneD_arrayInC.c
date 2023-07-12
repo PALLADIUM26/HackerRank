ce600b548d74702b564a6b8fa6715edd0a8fe624
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-07-12 09:53:19 +0530

    Update OneD_arrayInC.c

--- C/OneD_arrayInC.c
+++ C/OneD_arrayInC.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int arr[1000];
+    int n, i, sum = 0;
+    
+    scanf("%d",&n);    
+    
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }
+    
+    printf("%d",sum);
+    return 0;
+}
 
