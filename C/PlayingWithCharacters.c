147d1663599bb594babd7184b1f0272bc57f639f
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-07-13 00:04:58 +0530

    Update PlayingWithCharacters.c

--- C/PlayingWithCharacters.c
+++ C/PlayingWithCharacters.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    // char ch[100];
+    char ch;
+    char ch2[100];
+    char ch3[100];
+    scanf("%c",&ch);
+    scanf("\n");
+    // scanf("%[^\n]%*c", &ch);
+    scanf("%[^\n]%*c", &ch2);
+    scanf("%[^\n]%*c", &ch3);
+    
+    // printf(ch);
+    printf("%c",ch);
+    printf("\n");
+    printf(ch2);
+    printf("\n");
+    printf(ch3);
+    return 0;
+}
 
