b76b0c009298754cbc80cdd7cbac9b49a2867771
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-08-06 21:11:08 +0530

    Update JavaIntToString.java

--- Java/JavaIntToString.java
+++ Java/JavaIntToString.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class JavaIntToString {
+public class JavaIntToString { //change class name Solution
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
