13406754bae4e18faa9613a85cea2bb28b68494f
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-08-07 19:07:09 +0530

    Update JavaStringReverse.java

--- Java/JavaStringReverse.java
+++ Java/JavaStringReverse.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class JavaStringReverse { //change class name to Solution
+public class Solution{
 
     public static void main(String[] args) {
         
