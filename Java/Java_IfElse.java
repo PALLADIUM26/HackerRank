805d8877cb9dbcf43b16730a2c2c36f7189b722b
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-08-07 19:07:33 +0530

    Update Java_IfElse.java

--- Java/Java_IfElse.java
+++ Java/Java_IfElse.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Java_IfElse{ //change class name to Solution
+public class Solution{
 
     private static final Scanner sc = new Scanner(System.in);
 
