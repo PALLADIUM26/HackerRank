d229c369888751ee32aa31245ef38b58d7c39a91
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-08-09 08:35:32 +0530

    Update JavaMethodOverriding.java

--- Java/JavaMethodOverriding.java
+++ Java/JavaMethodOverriding.java
@@ -1,1 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Sports ob = new Sports();
+        System.out.println(ob.getName());
+        ob.getNumberOfTeamMembers();
+        Soccer ob2 = new Soccer();
+        System.out.println(ob2.getName());
+        ob2.getNumberOfTeamMembers();
+    }
+}
+
+class Sports{
+    String getName(){
+        return "Generic Sports";
+    }
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
 
