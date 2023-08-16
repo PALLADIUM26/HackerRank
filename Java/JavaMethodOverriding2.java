b0882347784737bd97299a35dadfab4cc0cb7734
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-08-16 20:23:52 +0530

    Update JavaMethodOverriding2.java

--- Java/JavaMethodOverriding2.java
+++ Java/JavaMethodOverriding2.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        motorcycle ob = new motorcycle();
+        ob.func();
+    }
+}
+
+class cycle{
+    void func(){}
+}
+
+class motorcycle extends cycle{
+    @Override
+    void func(){
+        System.out.println("Hello I am a motorcycle, I am a cycle with an engine.\nMy ancestor is a cycle who is a vehicle with pedals.");
+    }
+}
 
