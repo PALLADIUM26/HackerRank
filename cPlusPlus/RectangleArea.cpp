a95d4f66bed08b9c268fe553c63a2899e816b0e7
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-09-07 02:24:11 +0530

    Update RectangleArea.cpp

--- cPlusPlus/RectangleArea.cpp
+++ cPlusPlus/RectangleArea.cpp
@@ -1,1 +1,27 @@
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+
+class Rectangle{
+    public:
+        int l, b, area;
+        
+        void display(){
+            cout<<l<<" "<<b<<"\n";
+        }
+};
+
+class RectangleArea : public Rectangle{
+    public:
+        void read_input(){
+            cin>>l;
+            cin>>b;
+        }
+        
+        void display(){
+            cout<<l*b<<"\n";
+        }
+};
+
+
 
