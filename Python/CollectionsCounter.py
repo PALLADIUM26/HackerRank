75afdc03b4a4a7e7b113fc6fcd89209876f2fa66
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-11-11 01:21:00 +0530

    Update CollectionsCounter.py

--- Python/CollectionsCounter.py
+++ Python/CollectionsCounter.py
@@ -1,1 +1,21 @@
+# collections.Counter()
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x=int(input())
+d={}
+y=input().split()
+for i in y:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+n=int(input())
+s=0
+for i in range(n):
+    inp=input().split()
+    ni=inp[0]
+    xi=int(inp[1])
+    if ni in d and d[ni]>0:
+        d[ni]-=1
+        s+=xi
+print(s)
