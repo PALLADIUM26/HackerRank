603783558b4849ebc47fa42d56d8c07dd3392960
PALLADIUM26 <pranithdutta26@gmail.com>
2025-04-14 20:00:00 +0530

    input output 2

---
+++ Java/JavaStdinAndStdoutII.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        scan.useDelimiter("\r?\n");
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        String s=scan.next();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
